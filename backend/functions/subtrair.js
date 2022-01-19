7a4bb74adf68f4f65c8a4c0a94361c862d78dd06
Davi Guilherme <job.davic@gmail.com>
2022-01-19 15:54:32 -0300

    Soma, multiplicar, dividir adicionados

---
+++ backend/functions/subtrair.js
@@ -0,0 +1,4 @@
+module.exports = function Subtrair(n1,n2){
+    return n1-n2;
+
+}
--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,9 @@ const Port = 3000;
 
 
 const soma = require(path.join(__dirname,'functions','soma.js'));
+const subtrair = require(path.join(__dirname,'functions','subtrair.js'));
+const dividir = require(path.join(__dirname,'functions','dividir.js'))
+const multiplicar = require(path.join(__dirname,'functions','multiplicar.js'))
 app.use(parser.json());
 
 app.get('/',(req,res)=>{
@@ -18,6 +21,26 @@ app.post('/soma',(req,res)=>{
     
     res.send(JSON.stringify(result));
 })
+app.post('/subtrair',(req,res)=>{
+    let result = subtrair(req.body.n1,req.body.n2)
+
+    res.send(JSON.stringify(result));
+})
+app.post('/dividir',(req,res)=>{
+    
+    let result = dividir(req.body.n1,req.body.n2);
+    res.send(JSON.stringify(JSON.stringify(result)))
+
+})
+
+app.post('/multiplicar',(req,res)=>{
+    let result = multiplicar(req.body.n1,req.body.n2);
+    res.send(JSON.stringify(result));
+    
+
+})
+
+
 app.listen(Port,()=>{
     console.log(`Rodando na porta ${Port}`);
 })
