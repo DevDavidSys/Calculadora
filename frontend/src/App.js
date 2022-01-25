bbaa84923818e3cf1ef5492738215f1ffb376822
Davi Guilherme <job.davic@gmail.com>
2022-01-24 22:38:57 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,9 +6,8 @@ import React,{useState} from 'react';
 
 function App() {
   const [display,setDisplay] = useState('');
-  const [n1,setNone] = useState('');
-  const [n2,setNtwo] = useState('');
-  const [operator, setOperator ] = useState('');
+  
+  const [expression, setExpression ] = useState('');
   const [stage,setStage] = useState(0);
 
   const teclas = ['(',')','%','AC','7','8','9','/','4','5','6','*','1','2','3','-','0','.','=','+'];
@@ -29,45 +28,40 @@ function App() {
       if(item == '='){
         handleSubmit();
         setDisplay('');
-        setNone('');
-        setNtwo('');
-        setOperator('');
+        setExpression('');
         setStage(0);
       }
       else if(item == 'AC'){
         setDisplay('');
-        setNone('');
-        setNtwo('');
-        setOperator('');
+        setExpression('');
         setStage(0);
 
+      }
+      else if(item == '(' || item == ')'){
+        setDisplay(display + item);
+        setExpression(expression + item)
       }
     }
     else if(numbers.includes(item)){
       if(stage === 0){
-        setNone(n1+item);
-        setDisplay(n1 + item);
+        setDisplay(item);
+        setExpression(expression + item)
       }
       if(stage === 1){
-         setNtwo(n2 + item);
-         setDisplay(n2 + item)
       }
     }
     
     else if(operators.includes(item)){
-      setDisplay('');
-      setStage(1);
-      setOperator(item);
+      setExpression(expression + item);
       
     }
   
 
-    console.log(n1 +'   ' +operator + '    ' +n2 + stage)
   }
 
   const handleSubmit = ()=>{
     //envia os dados para o servidor
-    let send = JSON.stringify({none: n1,ntwo: n2,operator});
+    let send = JSON.stringify({expression});
     alert(send);
         
   }
@@ -75,7 +69,7 @@ function App() {
   return (
     <div className="App">
       <form className="calculator" onSubmit={handleSubmit}>
-        <div className="display">{display} </div>
+        <div className="display">{expression} </div>
         
         <div className="buttons">
           {buttons}
